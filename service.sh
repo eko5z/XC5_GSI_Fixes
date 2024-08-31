1bf35fd1e8db98ac2b4fec42a8c4d2796014cec5
Emil Kosz <ekosz@int.pl>
2024-08-31 18:43:24 +0200

    Service file was not updated with flashlight code

--- service.sh
+++ service.sh
@@ -2,15 +2,32 @@
 PRESS="00000001"; RELEASE="00000000"
 POWER="/dev/input/event1: 0001 0074"
 XCOVER="/dev/input/event6: 0001 00fc"
-
 # Events to catch.
-EVENTS="${POWER}${PRESS}|${XCOVER}${PRESS}|${XCOVER}${RELEASE}"
+EVENTS="${POWER} ${PRESS}|${XCOVER} ${PRESS}|${XCOVER} ${RELEASE}"
 
-/system/bin/getevent | /system/bin/grep -E "$EVENTS" |
+FLASH_FILE="/sys/class/camera/flash/rear_flash"
+
+/system/bin/getevent | /system/bin/grep -E "${EVENTS}" |
     {
 	while read LINE; do
-	    /system/bin/log -t Magisk -p i "[XC5_GSI_Fixes] Detected relevant event. Applying fix."
-	    /system/bin/echo check_connection > /sys/class/sec/tsp/cmd
-	    /system/bin/log -t Magisk -p i "[XC5_GSI_Fixes] Fix attempt result: $(/system/bin/cat /sys/class/sec/tsp/cmd_result)"
+	    case "${LINE}" in
+		"${POWER} ${PRESS}")
+		    # On power press, we do what febeslmeisl did. It
+		    # seems to work more reliably on power press than
+		    # the originally concieved release.
+		    /system/bin/log -t Magisk -p i "[XC5_GSI_Fixes] Detected relevant event. Applying fix."
+		    /system/bin/echo check_connection > /sys/class/sec/tsp/cmd
+		    /system/bin/log -t Magisk -p i "[XC5_GSI_Fixes] Fix attempt result: $(/system/bin/cat /sys/class/sec/tsp/cmd_result)"
+		    ;;
+		"${XCOVER} ${PRESS}")
+		    /system/bin/log -t Magisk -p i "[XC5_GSI_Fixes] XCover button pressed. Resetting timer."
+		    SECONDS=0 # Built-in shell timer.
+		    ;;
+		"${XCOVER} ${RELEASE}")
+		    [ "${SECONDS}" -ge 1 ] && echo "$((1-$(cat ${FLASH_FILE})))" > ${FLASH_FILE} \
+			&& /system/bin/log -t Magisk -p i "[XC5_GSI_Fixes] Toggled flashlight."
+		    ;;
+
+	    esac
 	done
     }
